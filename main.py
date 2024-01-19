d0c168e849bca42ebdeb1e05bf25a0f22b471f41
DESKTOP-1PGHF2C\flohe <flo.hess@online.de>
2024-01-19 16:08:52 +0100

    outlier detection and removal

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 import pandas as pd
 import numpy as np
+from scipy import stats
 
 diet = pd.read_csv('diet.csv')
 #print(diet.head())
-recipe = pd.read_csv('recipes.csv')
+recipes = pd.read_csv('recipes.csv')
 #print(recipe.head())
 requests = pd.read_csv('requests.csv')
 reviews = pd.read_csv('reviews.csv')
@@ -22,4 +23,25 @@ print(requests['HighCalories'].unique())
 
 print(requests.columns)
 
+recipes["Calories"] = recipes["Calories"].astype(int)
+recipes["FatContent"] = recipes["FatContent"].astype(int)
+recipes["SaturatedFatContent"] = recipes["SaturatedFatContent"].astype(int)
+recipes["FiberContent"] = recipes["FiberContent"].astype(int)
+recipes["SugarContent"] = recipes["SugarContent"].astype(int)
+recipes["ProteinContent"] = recipes["ProteinContent"].astype(int)
+recipes["CookTime"] = recipes["CookTime"].astype(int)
+recipes["PrepTime"] = recipes["PrepTime"].astype(int)
 
+
+requests["Time"] = requests["Time"].astype(int)
+
+
+#detect and remove outliers
+
+#exludes rows that have values outside a range of 3 standart deviations from the mean (using zscore methode)
+recipes= recipes[(np.abs(stats.zscore(recipes[['Calories','FatContent','SaturatedFatContent','FiberContent','SugarContent','ProteinContent']]))<3).all(axis=1)]
+#exludes rows that have values outside a range of 1 standart deviations from the mean (using zscore methode)
+recipes= recipes[(np.abs(stats.zscore(recipes[['CookTime']]))<1).all(axis=1)]
+#exludes rows that have values outside a range of 0.5 standart deviations from the mean (using zscore methode)
+recipes= recipes[(np.abs(stats.zscore(recipes[['PrepTime']]))<0.5).all(axis=1)]
+requests= requests[(np.abs(stats.zscore(requests[['Time']]))<0.5).all(axis=1)]
